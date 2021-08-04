778b4ac4736bb2f975414ba318ab75ea629e8c9b
phanxuanphucnd <phanxuanphucnd@gmail.com>
2021-08-04 07:52:38 +0700

    update

--- tests/train.py
+++ tests/train.py
@@ -1,12 +1,34 @@
-from aicovidvn.datasets import AICovidVNDataset
 from aicovidvn.models.cider_model import CIdeRModel
 from aicovidvn.learners.cider_learner import CIdeRLeaner
 
 def train():
-    
-    train_dataset = AICovidVNDataset(
+    depth_scale = 1
+    nfft = 1024
+    window_size = 8
+    sample_rate = 24000
+
+    model = CIdeRModel(
+        dropout=False,
+        depth_scale=depth_scale,
+        input_shape=(int(1024 * nfft / 2048), int(94 * window_size * sample_rate / 48000))
+    )
+
+    learner = CIdeRLeaner(
+        model=model
+    )
+    learner.train(
         root='./data/aivncovid-19',
-        mode='train',
-        eval_type='maj_vote',
-        transform=
+        window_size=window_size, 
+        n_nfft=nfft, 
+        sample_rate=sample_rate,
+        masking=True,
+        pitch_shift=True,
+        eval_type='maj_vote', 
+        noise=True,
+        batch_size=48,
+        learning_rate=0.0001,
+        n_epochs=100,
+        shuffle=True, 
+        save_dir='./models', 
+        model_name='aicovidbn'
     )
