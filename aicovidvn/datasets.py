cecd3009f42216cb5440de1feddb4b1f7836ef4d
phucphan <20156248@student.hust.edu.vn>
2021-08-07 13:51:51 +0700

    update

--- aicovidvn/datasets.py
+++ aicovidvn/datasets.py
@@ -76,7 +76,7 @@ class AICovidVNDataset(Dataset):
             num_freqs_to_mask = int(freq_percentage * all_freqs_num)
             f0 = np.random.uniform(low=0.0, high=all_freqs_num - num_freqs_to_mask)
             f0 = int(f0)
-            spec[:, f0:f0 + num_freqs_to_mask] = 0
+            spec[:, f0: f0 + num_freqs_to_mask] = 0
 
             time_percentage = random.uniform(0.0, time_masking_max_percentage)
 
